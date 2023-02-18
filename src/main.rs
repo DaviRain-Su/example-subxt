d59fc13cab4cd4947c41d1a6c0b84ea2febc6e02
Davirain <davirain.yin@gmail.com>
2023-02-18 16:46:54 +0800

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -10,8 +10,8 @@ use subxt::{
 use subxt::rpc::types::BlockNumber;
 use mmr_rpc::LeavesProof;
 
-#[subxt::subxt(runtime_metadata_path = "metadata/metadata.scale")]
-// #[subxt::subxt(runtime_metadata_url = "wss://rococo-rpc.polkadot.io:443")]
+// #[subxt::subxt(runtime_metadata_path = "metadata/metadata.scale")]
+#[subxt::subxt(runtime_metadata_url = "wss://rococo-rpc.polkadot.io:443")]
 pub mod polkadot {}
 
 /// Subscribe to all events, and then manually look through them and
@@ -39,14 +39,13 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     // test mmr_generateProof
     let method = "mmr_generateProof";
     // let block_numner: Vec<BlockNumber> = vec![0u32.into(),1u32.into(), 2u32.into()];
-    // let block_numner: Vec<BlockNumber> = vec![4007966u32.into(),4007967u32.into(),4007968u32.into()];
-    let block_numner: Vec<BlockNumber> = vec![30u64.into()];
+    let block_numner: Vec<BlockNumber> = vec![4007966u32.into(),4007967u32.into(),4007968u32.into()];
+    // let block_numner: Vec<BlockNumber> = vec![30u64.into()];
     let best_known_block_number = Option::<BlockNumber>::None;
     let at = Option::<sp_core::H256>::None;
     let params = rpc_params![block_numner, best_known_block_number, at];
     let result: LeavesProof<sp_core::H256> = api.rpc().request(method, params).await?;
     println!("mmr_generateProof is {:?}", result);
-
 
     Ok(())
 }
